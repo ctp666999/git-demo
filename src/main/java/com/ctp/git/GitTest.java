30eb5bfe31d96fbf7f9d347e988bfc48bacae301
ctp <446673255@qq.com>
2022-05-24 20:44:12 +0800

    hot-fix-0.1.1

--- src/main/java/com/ctp/git/GitTest.java
+++ src/main/java/com/ctp/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git3!");
+        System.out.println("Hello git4!");
     }
 }
