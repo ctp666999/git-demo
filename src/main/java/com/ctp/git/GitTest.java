1dcc868ab6d620456d2431002473c58270aea6c4
ctp <446673255@qq.com>
2022-05-24 20:54:25 +0800

    hot-fix-0.1.3

--- src/main/java/com/ctp/git/GitTest.java
+++ src/main/java/com/ctp/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello git!");
         System.out.println("Hello git3!");
         System.out.println("Hello git4!");
+        System.out.println("乱来!");
     }
 }
